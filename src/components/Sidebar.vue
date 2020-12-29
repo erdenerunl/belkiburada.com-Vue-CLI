0be80ce9d8aa574e6c70348e7c9fc059af1e9f6f
erdenerunl <erdenerunale@gmail.com>
2020-12-29 23:30:33 +0300

    Navbar, sidebar and routes were set.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <router-link :to="`/categories/${categoryID}`" class="nav-link">
+              Computer
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="`/categories/${categoryID}`" class="nav-link">
+              Automobile
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="`/categories/${categoryID}`" class="nav-link">
+              Smartphone
+            </router-link>
+          </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
